b51307acba7fd4b56508206075dd919ab684dab5
AndrewLondon <andrewmart@mail.com>
2024-02-25 02:30:39 +0000

    migration chat table created

---
+++ postgres/20240225021723_chat_users_table.sql
@@ -0,0 +1,29 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS chats (
+    id BIGSERIAL PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_chats (
+    chat_id BIGINT REFERENCES chats(id),
+    user_id BIGINT NOT NULL CHECK (user_id > 0),
+    PRIMARY KEY (chat_id, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS chats_messages (
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT REFERENCES chats(id),
+    user_id BIGINT NOT NULL CHECK (user_id > 0),
+    text TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS chats_messages;
+DROP TABLE IF EXISTS users_chats;
+DROP TABLE IF EXISTS chats;
+-- +goose StatementEnd
+
