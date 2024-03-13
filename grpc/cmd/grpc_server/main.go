e5c7d72d911c75eb23472d93595c229e6619df4c
AndrewLondon <andrewmart@mail.com>
2024-03-13 21:39:13 +0000

    Updated chat-server

--- grpc/cmd/grpc_server/main.go
+++ grpc/cmd/grpc_server/main.go
@@ -21,29 +21,15 @@ type server struct {
 
 func (srv *server) Create(ctx context.Context, req *desc.CreateNewChatRequest) (*desc.CreateNewChatResponse, error) {
 	log.Printf("Create New Chat request received: %v", req)
-
-	//For testing purposes
-	// response := &chat_server_v1.CreateNewChatResponse{
-	// 	Id: 1345,
-	// }
-	// return response, nil
 	return &desc.CreateNewChatResponse{
 		Id: gofakeit.Int64(),
 	}, nil
-
 }
 
-func (srv *server) Delete(ctx context.Context, req *desc.DeleteChatRequest) (emptypb.Empty, error) {
+func (srv *server) Delete(ctx context.Context, req *desc.DeleteChatRequest) (*emptypb.Empty, error) {
 	log.Printf("Delete Chat request received: %v", req)
-	return nil
-
+	return &emptypb.Empty{}, nil
 }
-
-//func (srv *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*desc.SendMessageResponse, error) {
-//	log.Printf("SendMessageRequest received: %v", req)
-//
-//	return &desc.SendMessageResponse{SendMessageResponse: &empty.Empty{}}, nil
-//}
 
 func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
@@ -59,7 +45,6 @@ func main() {
 	// and gRPC's web-based GUI (grpcui) to inspect the server's
 	// services and make RPC calls without needing to know
 	// the specifics of each service beforehand.
-
 	reflection.Register(srv)
 	desc.RegisterChatAPIServicesV1Server(srv, &server{})
 
@@ -68,5 +53,4 @@ func main() {
 	if err = srv.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-
 }
