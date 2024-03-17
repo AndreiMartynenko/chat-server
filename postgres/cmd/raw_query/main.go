4d5dc0ec2d9e6da075248613e9561a5c3f3961a4
AndrewLondon <andrewmart@mail.com>
2024-03-17 20:55:27 +0000

    Updated chat server

---
+++ postgres/cmd/raw_query/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "log"
+import "context"
+import "github.com/brianvoe/gofakeit"
+import "github.com/jackc/pgx/v4"
+
+const (
+	dbDSN = "host=localhost port=54321 dbname=chats user=chat-user password=chat-password sslmode=disable"
+)
+
+func main() {
+	ctx := context.Background()
+
+	//Create a new connection to the database
+	con, err := pgx.Connect(ctx, dbDSN)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	defer con.Close(ctx)
+
+	// Make a query to the database
+	res, err := con.Exec(ctx, "INSERT INTO chats (id) VALUES ($1)", gofakeit.UUID())
+	if err != nil {
+		log.Fatalf("failed to insert chat : %v", err)
+	}
+
+	log.Printf("inserted chat with id: %s", res.RowsAffected())
+
+	// Make a query to the database to get data from the chat
+
+	rows, err := con.Query(ctx, "SELECT id FROM chats")
+	if err != nil {
+		log.Fatalf("failed to get chat : %v", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id string
+		err = rows.Scan(&id)
+		if err != nil {
+			log.Fatalf("failed to scan chat : %v", err)
+		}
+		log.Printf("chat id: %s", id)
+	}
+
+}
