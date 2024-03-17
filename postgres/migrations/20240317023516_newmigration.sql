5639f8206cfdc80790743212f41e5e7d0f53a990
AndrewLondon <andrewmart@mail.com>
2024-03-17 04:02:02 +0000

    Updated chat server

--- postgres/migrations/20240317023516_newmigration.sql
+++ postgres/migrations/20240317023516_newmigration.sql
@@ -2,21 +2,21 @@
 -- +goose StatementBegin
 
 create table if not exists chats (
-                                     id bigserial primary key
+    id bigserial primary key
 );
 
 create table if not exists chats_users (
-                                           chat_id bigserial references chats,
-                                           user_id bigint not null,
-                                           primary key (chat_id, user_id)
+    chat_id bigserial references chats,
+    user_id bigint not null,
+    primary key (chat_id, user_id)
 );
 
 create table if not exists chats_messages (
-                                              id bigserial primary key,
-                                              chat_id bigint references chats,
-                                              user_id bigint not null,
-                                              body text not null,
-                                              time timestamp with time zone not null
+    id bigserial primary key,
+    chat_id bigint references chats,
+    user_id bigint not null,
+    body text not null,
+    time timestamp with time zone not null
 );
 -- +goose StatementEnd
 
