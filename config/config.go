568c30324e694f37744d3eff3e7e4e73f5794aed
AndrewLondon <andrewmart@mail.com>
2024-04-10 16:17:18 +0100

    Config updated

--- config/config.go
+++ config/config.go
@@ -5,3 +5,11 @@ const (
 	EnvDev   = "dev"
 	EnvProd  = "prod"
 )
+
+type Metrics struct {
+	ServiceName string `json:"service_name" env-default:"chat-server"`
+}
+
+type Server struct {
+	Port int `json:"port" env:"SERVER_PORT" env-default:"50001"`
+}
